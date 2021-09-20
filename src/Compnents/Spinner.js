d03e7f75843016eeba4c77439cd5c2ca623821f9
Arish82 <arish123.ahamad@gmail.com>
2021-09-20 21:06:34 +0530

    Adding all the app

---
+++ src/Compnents/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const Spinner= ()=>{
+    return (
+        <div className="text-center">
+            <img style={{width: "10%"}} src="https://i.gifer.com/74H8.gif" alt="" />
+        </div>
+    )
+}
+
+export default Spinner;
+
